c11915cbd59e892122824e84428047385c622987
amanda23z <amanda.leme@aluno.senai.br>
2023-08-17 17:03:47 -0300

    Eviando as atividades feitas em aula

--- todolist/src/main/java/br/com/todolist/TodoController.java
+++ todolist/src/main/java/br/com/todolist/TodoController.java
@@ -1,9 +1,11 @@
 package br.com.todolist;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/todos")
@@ -21,4 +23,25 @@ private final TodoRepository TodoRepo;
     public List<Todo> getAll() {
         return this.TodoRepo.findAll();
     }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Todo create(@RequestBody Todo tarefas) {
+        return this.TodoRepo.save(tarefas);
+    }
+
+@DeleteMapping("/{tarefaId}")
+    public ResponseEntity<Void> delete(@PathVariable Integer tarefaId) {
+        Optional<Todo> todo = this.TodoRepo.findById(tarefaId);
+        if (todo.isPresent()) {
+            this.TodoRepo.deleteById(tarefaId);
+            return ResponseEntity.noContent().build();
+
+        } else {
+            return ResponseEntity.notFound().build();
+
+        }
+
+}
+
 }
